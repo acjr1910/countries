476aa86b9b1aba2ac7c01bc517ce89a63776c807
Agnaldo Cardoso Junior <acjr.developer@gmail.com>
2020-05-04 22:52:56 -0300

    testing array to obj due big O linear time

--- src/containers/Contries/Countries.tsx
+++ src/containers/Contries/Countries.tsx
@@ -1,6 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Countries() {
+  const [countryList, setCountryList] = useState([]);
+  const [countryObj, setCountryObj] = useState({});
+
+  useEffect(() => {
+    async function getCountries() {
+      const data = await fetch(`https://restcountries.eu/rest/v2/all`);
+      const json = await data.json();
+      setCountryList(json);
+      return json;
+    }
+    getCountries();
+  }, []);
+
+  console.log(countryList);
+
+  function makeCountriesObject() {
+    var obj = {};
+    countryList.map(function (country: {
+      name: string;
+      subregion: string;
+    }): void {
+      const s = String(country.name).trim();
+      var newObject = {
+        [s]: {
+          name: country.name,
+          subregion: country.subregion,
+        },
+      };
+      obj = Object.assign({}, obj, newObject);
+    });
+    console.log(obj);
+    var arr = Object.keys(obj);
+    console.log(arr);
+    return obj;
+  }
+
+  makeCountriesObject();
+
   return <div>Countries</div>;
 }
 
