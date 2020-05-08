2b38c7defc8b63577ae948176c988001c82f71c2
Agnaldo Cardoso Junior <acjr.developer@gmail.com>
2020-05-08 12:44:26 -0300

    header is donw

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
+import './utils/styles/reset.css';
+import './utils/styles/font.css';
 
 ReactDOM.render(
   <React.StrictMode>
