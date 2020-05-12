5eda644d69eb8b77ae240d1a577b488f386cc1e3
Agnaldo Cardoso Junior <acjr.developer@gmail.com>
2020-05-12 15:58:51 -0300

    desktop styles

--- src/components/Countries/Countries.styles.tsx
+++ src/components/Countries/Countries.styles.tsx
@@ -1,10 +1,14 @@
 import styled from 'styled-components';
 
 export const Styles = styled.div`
+  width: 100%;
   .countries {
     &__cards-container {
       display: flex;
       flex-wrap: wrap;
+      @media (min-width: 768px) {
+        justify-content: space-between;
+      }
     }
   }
 `;
