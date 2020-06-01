fee8b4d83813375a0fd130a4ef9bca1df38d60bb
Agnaldo Cardoso Junior <acjr.developer@gmail.com>
2020-06-01 19:58:24 -0300

    added auto-fit in grid template-columns

--- src/components/Countries/Countries.styles.tsx
+++ src/components/Countries/Countries.styles.tsx
@@ -9,7 +9,7 @@ export const Styles = styled.div`
       @media (min-width: 768px) {
         justify-content: flex-start;
         display: grid;
-        grid-template-columns: repeat(4, 1fr);
+        grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
         justify-items: center;
         grid-gap: 0 3rem;
       }
